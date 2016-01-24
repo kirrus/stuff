477b1826dcf34d795e6fd612b611d61aebfcd348
kirrus <kirrus@kirrus.co.uk>
2016-01-24 19:17:12 +0000

    Cribbed from borg backup's example

---
+++ borg-backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# borg init --encryption=keyfile $user@$host:borg/
+
+BORG="/usr/local/bin/borg-linux64"
+
+REPOSITORY="$USER"@"$HOST":borg/
+export BORG_PASSPHRASE="Your phrase here."
+
+if [ ! -x $BORG ]; then
+        echo "I can't execute borg."
+        exit 1
+fi
+
+
+$BORG create --stats --compression zlib,8       \
+    "$REPOSITORY"::$(hostname)-$(date +%Y-%m-%d)    \
+    /home                                       \
+    /var/www                                    \
+    --exclude /home/kirrus/archive              \
+    --exclude '/home/*/logs'                    \
+    --exclude '*.pyc'                           \
+    --exclude '/var/www/owncloud/data/*/thumbnails/'
+
+$BORG prune -v "$REPOSITORY" --prefix $(hostname)- \
+    --keep-daily=7 --keep-weekly=4 --keep-monthly=6
